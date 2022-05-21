3f94394413994dfecaddd43ce5e7876153d49e16
tinaytini <tinaytini@yahoo.com>
2022-05-21 23:19:59 +0200

    login set up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,29 @@
 
-import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'; 
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithRedirect } from 'firebase/auth'
+import firebaseApp from "../firebase";  
+
+const auth = getAuth(firebaseApp)
+
 function Login() {
     return (
         <div id="login-page">
             <div id="login-card">
                 <h2>Let's Chat</h2>
-                <div className='login-button google'>
+                <div 
+                    className='login-button google'
+                    onClick={() => signInWithRedirect(auth, new GoogleAuthProvider())}
+                >
                     <GoogleOutlined/> Login with Google
                 </div>
                 <br/><br/>
-                <div className='login-button google'> 
+                <div 
+                    className='login-button google'
+                    onClick={() => signInWithRedirect(auth, new FacebookAuthProvider())}
+                > 
                     <FacebookOutlined/> Login with FaceBook
                 </div>
-            </div>
+            </div> 
         </div>
     )
 }
